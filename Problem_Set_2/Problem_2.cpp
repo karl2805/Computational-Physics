ebf0326936f0db752c5d1c95cc32b44022e98a62
karl2805 <karlriedewald@gmail.com>
2025-10-01 23:57:47 +0100

    problem 2

--- Problem_Set_2/Problem_2.cpp
+++ Problem_Set_2/Problem_2.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+double f(double x)
+{
+    return x*x - x + 1;
+    
+}
+double CentralDifferentiate(double x, double dx, double (*InputFunction)(double))
+{
+    return (InputFunction(x + dx) - InputFunction(x - dx)) / (2 * dx);
+}
+
 int main()
 {
-    std::cout << "git test\n";
+    std::cout << CentralDifferentiate(2, 5, f) << std::endl;
+    
+    
 }
