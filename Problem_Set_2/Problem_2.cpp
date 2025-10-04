b00f9e641ff9c0be28c839aca7ff00e07b961f8f
karl2805 <karlriedewald@gmail.com>
2025-10-04 01:23:22 +0100

    Problem 2 code finished

--- Problem_Set_2/Problem_2.cpp
+++ Problem_Set_2/Problem_2.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+#include <iomanip>
+
+#define LINEBREAK std::cout << "------------------------------------------------------------------\n";
 
 double f(double x)
 {
     return x*x - x + 1;
-    
 }
+
 double CentralDifferentiate(double x, double dx, double (*InputFunction)(double))
 {
     return (InputFunction(x + dx) - InputFunction(x - dx)) / (2 * dx);
 }
 
 int main()
-{
-    std::cout << CentralDifferentiate(2, 5, f) << std::endl;
+{   
+    LINEBREAK;
+    std::cout << "Derivative approximation f'(2) with varying step size del_x: \n";
+
+    LINEBREAK;
+
+    std::cout << std::left << "del_x" << "\t" << "f'(2)" << '\t' << "Abs true error" << std::endl;
+
+    double dir = CentralDifferentiate(2, 0.25, f);
+    
+    std::cout << std::left << 0.25 << '\t' << dir << '\t' << std::abs(3 - dir) / 3 << std::endl;
+
+    dir = CentralDifferentiate(2, 0.125, f);
+
+    std::cout << std::left << 0.125 << '\t' << dir << '\t' << std::abs(3 - dir) / 3 << std::endl;
+
+
+  
+
     
     
 }
